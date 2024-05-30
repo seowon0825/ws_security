caad4e96d36cbb613498b2122fa2ae9747238355
Bae seowon <bsw0825@naver.com>
2024-05-30 11:56:06 +0900

    3

--- sec_jpa_thymeleaf/src/main/java/com/example/sec_jpa_thymeleaf/config/SecurityConfig.java
+++ sec_jpa_thymeleaf/src/main/java/com/example/sec_jpa_thymeleaf/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.example.sec_jpa_thymeleaf.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -12,5 +14,11 @@ public class SecurityConfig {
 	@Bean
 	public BCryptPasswordEncoder bCryPasswordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		
+		return http.build();
 	}
 }
